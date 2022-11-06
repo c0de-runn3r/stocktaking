265a74e55cd35049d366a93fe0ce7d7afe27ce92
c0de-runn3r <109829568+c0de-runn3r@users.noreply.github.com>
2022-11-06 11:12:21 +0100

    logging

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import logging
 from aiogram import executor
 from create_bot import dp
 
@@ -10,5 +11,6 @@ client.register_handlers_client(dp)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_statup)
+    logging.basicConfig(level=logging.DEBUG)
 
 
